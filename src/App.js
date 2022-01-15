b923f5c4a52c279752bbeca04d107154eb6658e3
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-01-15 21:25:18 +0000

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+//import { Auth } from "aws-amplify";
+
+/*async function signOut() {
+  try {
+    await Auth.signOut();
+  } catch (error) {
+    console.log("error signing out: ", error);
+  }
+}*/
 
 function App() {
   return (
@@ -8,8 +19,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from v2</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
